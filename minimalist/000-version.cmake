60e4ac24a7c2cb18816d161c88fec953113d1e43
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-08 03:16:31 +0300

    update (0.0.8) 001-symptoms

--- minimalist/000-version.cmake
+++ minimalist/000-version.cmake
@@ -1,10 +1,11 @@
 
-# 2021y-02m-13d. WorkSpace project.
+# [2021y-02m-13d][00:00:00] WorkSpace project.
+# [2021y-03m-07d][07:28:59] Idrisov D. R.
 ################################################################################
 
 set(gMINIMALIST_VERSION_MAJOR 0)
 set(gMINIMALIST_VERSION_MINOR 0)
-set(gMINIMALIST_VERSION_PATCH 7)
+set(gMINIMALIST_VERSION_PATCH 8)
 
 set(gMINIMALIST_VERSION "${gMINIMALIST_VERSION_MAJOR}.${gMINIMALIST_VERSION_MINOR}.${gMINIMALIST_VERSION_PATCH}")
 
